6e3127941f225654fb30476ed745bf13a29967d6
Oliver Francis <olliefrancis97@hotmail.co.uk>
2022-03-01 11:04:03 +0000

    Initialising frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class = "heroSection ">
+          <div id= "expandable" class = "animate__animated animate__fadeInLeft">
+              zk properties.
+          </div>
+      </div>
+
+      <div class = "introSection">
+
+          <div class = "introContainer">
+              {/* <img src="./client/images/buildingsStreet.jpeg" alt=""> */}
+              <h2><u>about.</u> </h2>
+
+              <p>zk properties is a decentralised property listing application backed by the ethereum blockchain. we tokenise property listing, making the purchasing of an asset fast, secure, and with fewer hidden costs.</p>    
+              {/* <p>through the use zkSNARKS we are able to securely prove ownership of a property without the need for a third party intermediary. Making the process less expensive for you.</p> */}
+          </div>
+
+      </div>
+
+      <div class = "explainSection">
+          <div class = "explainContainer">
+              <h2><u>how it works. </u> </h2>
+
+              <p>our available listing are on opensea, an open source NFT marketplace. 
+                  through the use zkSNARKS we are able securely prove the ownership of a property without the need for a third party intermediary. Making the process cheaper.</p>
+          </div>
+
+      </div>
+    </>
   );
 }
 
